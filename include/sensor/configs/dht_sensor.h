156bdd1ee0391bcf8ad5baff281c0feeabdfa432
izasoerya <ihza.surya@gmail.com>
2026-08-12 19:06:23 +0700

    feat: working module class

---
+++ include/sensor/configs/dht_sensor.h
@@ -0,0 +1,59 @@
+#ifndef DHT_READING_H
+#define DHT_READING_H
+
+#include <Arduino.h>
+#include <DHT.h>
+
+class DHTSensor
+{
+public:
+    DHTSensor(const uint8_t pinDHT) : _pinDHT(pinDHT), _dht(pinDHT, DHT22) {}
+    ~DHTSensor() {}
+
+    uint8_t begin()
+    {
+        _dht.begin();
+        float temp = _dht.readTemperature();
+        if (isnan(temp))
+        {
+            Serial.println("DHT22 read failed");
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    float getTemperature()
+    {
+        float val = _dht.readTemperature();
+        if (isnan(val))
+        {
+            return 0.0;
+        }
+        else
+        {
+            return val;
+        }
+    }
+
+    float getHumidity()
+    {
+        float val = _dht.readHumidity();
+        if (isnan(val))
+        {
+            return 0.0;
+        }
+        else
+        {
+            return val;
+        }
+    }
+
+private:
+    const uint8_t _pinDHT;
+    DHT _dht;
+};
+
+#endif
---
+++ src/projects/sibob/bang-bang.h
@@ -0,0 +1,107 @@
+#ifndef BANG_BANG_H
+#define BANG_BANG_H
+
+#include <Arduino.h>
+
+struct BangBangConfig
+{
+    float upperTemp;
+    float bottomTemp;
+    float upperHum;
+    float bottomHum;
+};
+
+struct ActuatorConfig
+{
+    uint8_t pinExhaustFan;
+    uint8_t pinMistMaker;
+    uint8_t pinHeater;
+};
+
+class BangBangController
+{
+private:
+    BangBangConfig _configSetPoint;
+    ActuatorConfig _configActuator;
+
+public:
+    // Lazy separate body file sorry
+    BangBangController(const BangBangConfig setPointConfig,
+                       const ActuatorConfig actuatorConfig)
+    {
+        _configSetPoint = setPointConfig;
+        _configActuator = actuatorConfig;
+    }
+    ~BangBangController() {}
+
+    void control(float temperature, float humidity) const
+    {
+        if (temperature > _configSetPoint.upperTemp)
+        {
+            // Turn off exhaust fan
+            digitalWrite(_configActuator.pinExhaustFan, HIGH);
+            Serial.print("FAN IS ON  pin: ");
+            Serial.println(_configActuator.pinExhaustFan);
+        }
+        else if (temperature < _configSetPoint.bottomTemp)
+        {
+            // Turn on exhaust fan
+            digitalWrite(_configActuator.pinExhaustFan, LOW);
+            Serial.print("FAN IS OFF pin: ");
+            Serial.println(_configActuator.pinExhaustFan);
+        }
+
+        if (humidity > _configSetPoint.upperHum)
+        {
+            // Turn off mist maker
+            digitalWrite(_configActuator.pinMistMaker, LOW);
+            Serial.print("MIST IS OFF  pin: ");
+            Serial.println(_configActuator.pinMistMaker);
+
+            // Turn off exhaust fan
+            digitalWrite(_configActuator.pinExhaustFan, HIGH);
+            Serial.print("FAN IS ON  pin: ");
+            Serial.println(_configActuator.pinExhaustFan);
+        }
+        else if (humidity < _configSetPoint.bottomHum)
+        {
+            // Turn on mist maker
+            digitalWrite(_configActuator.pinMistMaker, HIGH);
+            Serial.print("MIST IS ON  pin: ");
+            Serial.println(_configActuator.pinMistMaker);
+
+            // Turn on exhaust fan
+            digitalWrite(_configActuator.pinExhaustFan, LOW);
+            Serial.print("FAN IS OFF pin: ");
+            Serial.println(_configActuator.pinExhaustFan);
+        }
+    }
+
+    void controlHeater(float temperature, float humidity, bool forcedOff) const
+    {
+        static bool heatDemand = false;
+        if (temperature < _configSetPoint.bottomTemp)
+        {
+            heatDemand = true;
+        }
+        else if (temperature >= _configSetPoint.upperTemp)
+        {
+            heatDemand = false;
+        }
+
+        if (forcedOff || !heatDemand)
+        {
+            analogWrite(_configActuator.pinHeater, 0);
+            Serial.print("HEATER IS OFF  pin: ");
+            Serial.println(_configActuator.pinHeater);
+        }
+        else
+        {
+            analogWrite(_configActuator.pinHeater, 30);
+            Serial.print("HEATER IS ON  pin: ");
+            Serial.println(_configActuator.pinHeater);
+        }
+    }
+};
+
+#endif
