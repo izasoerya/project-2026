2585859779359b97ae8634bca0a92469071f8482
izasoerya <ihza.surya@gmail.com>
2026-09-18 15:53:31 +0700

    fix: set hostname as static so other task can see it

--- src/projects/brin-bandung/slave-ws/slave.cpp
+++ src/projects/brin-bandung/slave-ws/slave.cpp
@@ -26,9 +26,9 @@ void setup()
 {
     Serial.begin(115200);
 
-    char hostname[64];
-    snprintf(hostname, sizeof(hostname), "slave-arr-bandung-persemaian-%d.local", DEVICE_ID + 1);
-    static WiFiModule wifi(ssid, password, hostname, WIFI_POWER_19_5dBm);
+    static char hostname[64];
+    snprintf(hostname, sizeof(hostname), "slave-arr-bandung-persemaian-%d", DEVICE_ID + 1);
+    static WiFiModule wifi(ssid, password, hostname, WIFI_POWER_8_5dBm);
     static contextDaemon daemonCtx(wifi);
     daemonCtx.setInternetStatus(FeatureStatus::INTERNET); // Default to not working
     daemonCtx.setNTPStatus(FeatureStatus::NTP);           // Default to not working
@@ -38,10 +38,10 @@ void setup()
     static contextTHWS sensorCtx(Wire);
     static contextMB mbCtx(Serial1);
     static contextWD wdCtx(Serial0);
-    xTaskCreate(Application::taskReadTHWS, "sampling THWS task", 8192, &sensorCtx, 3, &handleReadTHWS);
-    xTaskCreate(Application::taskReadWD, "sampling WD task", 8192, &wdCtx, 4, &handleReadWD);
-    xTaskCreate(Application::taskMBSlave, "modbus slave task", 8192, &mbCtx, 1, &handleMBSlave);
-    xTaskCreate(Application::taskPollOta, "ota task", 8192, nullptr, 2, &handleOta);
+    xTaskCreate(Application::taskReadTHWS, "sampling THWS task", 4096, &sensorCtx, 3, &handleReadTHWS);
+    xTaskCreate(Application::taskReadWD, "sampling WD task", 3072, &wdCtx, 4, &handleReadWD);
+    xTaskCreate(Application::taskMBSlave, "modbus slave task", 4096, &mbCtx, 1, &handleMBSlave);
+    xTaskCreate(Application::taskPollOta, "ota task", 4096, &daemonCtx, 2, &handleOta);
     xTaskCreate(Application::taskDaemon, "daemon task", 4096, &daemonCtx, 1, &handleDaemon);
 }
 
