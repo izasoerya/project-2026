5d3155eaca5df7616eccbef7dac7d9e86bc3bb40
izasoerya <ihza.surya@gmail.com>
2026-09-18 17:02:54 +0700

    feat: parser command for webserial

---
+++ src/projects/brin-bandung/master/services/command_parser.h
@@ -0,0 +1,43 @@
+#if !defined(COMMAND_PARSER_H)
+#define COMMAND_PARSER_H
+
+#include <Arduino.h>
+#include "../utils/enum.h"
+
+class CommandParser
+{
+public:
+    static EnabledOTA otaCommand(uint8_t *data, size_t len)
+    {
+        char buffer[256];
+        if (len >= sizeof(buffer))
+            len = sizeof(buffer) - 1;
+        memcpy(buffer, data, len);
+        buffer[len] = '\0';
+
+        if (strcmp(buffer, "ENABLE_OTA_SLAVE_WS"))
+        {
+            Serial.printf("[INFO] Message Valid: %s\n", buffer);
+            return EnabledOTA::SLAVE_WS_ON;
+        }
+        else if (strcmp(buffer, "DISABLE_OTA_SLAVE_WS"))
+        {
+            Serial.printf("[INFO] Message Valid: %s\n", buffer);
+            return EnabledOTA::SLAVE_WS_OFF;
+        }
+        else if (strcmp(buffer, "ENABLE_OTA_SLAVE_ARR"))
+        {
+            Serial.printf("[INFO] Message Valid: %s\n", buffer);
+            return EnabledOTA::SLAVE_ARR_ON;
+        }
+        else if (strcmp(buffer, "DISABLE_OTA_SLAVE_ARR"))
+        {
+            Serial.printf("[INFO] Message Valid: %s\n", buffer);
+            return EnabledOTA::SLAVE_ARR_OFF;
+        }
+        Serial.printf("[ERROR] Message Invalid: %s\n", buffer);
+        return EnabledOTA::INVALID;
+    }
+};
+
+#endif // COMMAND_PARSER_H
