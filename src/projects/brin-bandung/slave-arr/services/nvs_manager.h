3b34fc11e2c87ee13fd558b27d2c2a20fe430ede
izasoerya <ihza.surya@gmail.com>
2026-09-19 16:20:48 +0700

    feat: chekcer if the element is empty or not

--- src/projects/brin-bandung/slave-arr/services/nvs_manager.h
+++ src/projects/brin-bandung/slave-arr/services/nvs_manager.h
@@ -5,23 +5,77 @@
 class NVSManager
 {
 public:
+    static bool ensureRainfall(float defaultValue = 0.0F)
+    {
+        Preferences prefs;
+        if (!prefs.begin("app_config", false))
+            return false;
+
+        if (!prefs.isKey("rainfall_counter") && prefs.putFloat("rainfall_counter", defaultValue) == 0)
+        {
+            prefs.end();
+            return false;
+        }
+
+        prefs.end();
+        return true;
+    }
+
     static bool storeRainfall(float val)
     {
         Preferences prefs;
-        prefs.begin("app_config", false);
+        if (!prefs.begin("app_config", false))
+            return false;
+
         prefs.putFloat("rainfall_counter", val);
         float counter = prefs.getFloat("rainfall_counter", -1);
         prefs.end();
-        if (val == counter)
-            return true;
-        return false;
+        return val == counter;
+    }
+
+    static bool storeTest(float val)
+    {
+        Preferences prefs;
+        if (!prefs.begin("app_config", false))
+            return false;
+
+        prefs.putFloat("test", val);
+        float counter = prefs.getFloat("test", -1);
+        prefs.end();
+        return val == counter;
     }
 
     static float getRainfall()
     {
         Preferences prefs;
-        prefs.begin("app_config", true);
+        if (!prefs.begin("app_config", true))
+            return -1;
+
+        if (!prefs.isKey("rainfall_counter"))
+        {
+            prefs.end();
+            return -1;
+        }
+
         float counter = prefs.getFloat("rainfall_counter", -1);
+        prefs.end();
+        return counter;
+    }
+
+    static float getTest()
+    {
+        Preferences prefs;
+        if (!prefs.begin("app_config", true))
+            return -1;
+
+        if (!prefs.isKey("test"))
+        {
+            prefs.end();
+            return -1;
+        }
+
+        float counter = prefs.getFloat("test", -1);
+        prefs.end();
         return counter;
     }
 };
