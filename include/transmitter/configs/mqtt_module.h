bfe27d8430418c7d6ac4d9a61c1033e87bf678ab
izasoerya <ihza.surya@gmail.com>
2026-09-26 00:09:27 +0700

    chore: refactor to pubsubsclient library

--- include/transmitter/configs/mqtt_module.h
+++ include/transmitter/configs/mqtt_module.h
@@ -2,7 +2,8 @@
 #define MQTT_MODULE_H
 
 #include <WiFi.h>
-#include <espMqttClient.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include "../base_transmitter.h"
 
 class MQTTModule
@@ -10,65 +11,70 @@ class MQTTModule
 private:
     static const uint8_t _qos = 0;
     static const bool _retain = false;
-    static const uint16_t _port = 1883;
 
     const char *_username;
     const char *_password;
     const char *_brokerUrl;
+    const uint16_t _port;
 
-    espMqttClient _mqttClient;
+    WiFiClientSecure _wifiClient;
+    PubSubClient _mqttClient;
+
+    void (*_userCallback)(const char *topic, const char *payload) = nullptr;
 
 public:
     MQTTModule(
-        const char *username, const char *password, const char *brokerUrl)
-        : _username(username), _password(password), _brokerUrl(brokerUrl) {}
-
+        const char *username, const char *password, const char *brokerUrl, const uint16_t port)
+        : _username(username), _password(password), _brokerUrl(brokerUrl), _port(port),
+          _mqttClient(_wifiClient)
+    {
+        _wifiClient.setInsecure();
+        _mqttClient.setServer(_brokerUrl, _port);
+    }
     ~MQTTModule() = default;
 
     bool isConnected() { return _mqttClient.connected(); }
 
     bool connect()
     {
-        _mqttClient.setServer(_brokerUrl, _port);
-        if (!_mqttClient.connect())
-            return false;
-        else
-            return true;
+        bool result = _mqttClient.connect("ESP32Client", _username, _password);
+        if (!result)
+        {
+            Serial.printf("MQTT connect failed. State: %d\n", _mqttClient.state());
+            // State codes: -4=CONNECT_FAILED, -3=CONNECT_BAD_PROTOCOL, -2=CONNECT_BAD_CLIENT_ID
+            //              -1=CONNECT_UNAVAILABLE, 0=CONNECT_BAD_CREDENTIALS, 1=CONNECTED
+        }
+        return result;
     }
 
     bool reconnect()
     {
-        if (!_mqttClient.connected())
-        {
-            bool success = _mqttClient.connect();
-            return success;
-        }
-        return true;
+        if (_mqttClient.connect("ESP32Client", _username, _password))
+            return true;
+        return false;
     }
 
     bool disconnect()
     {
-        bool success = _mqttClient.disconnect();
-        return success;
+        _mqttClient.disconnect();
+        return true;
     }
 
     void onMessage(void (*_msgCallback)(const char *topic, const char *payload))
     {
-        _mqttClient.onMessage(
-            [&](const espMqttClientTypes::MessageProperties &properties,
-                const char *topic, const uint8_t *payload, size_t len,
-                size_t index, size_t total)
-            {
-                char buffer[len + 1];
-                memcpy(buffer, payload, len);
-                buffer[len] = '\0';
-                _msgCallback(topic, buffer);
-            });
+        _userCallback = _msgCallback;
+        _mqttClient.setCallback([this](char *topic, byte *payload, unsigned int length)
+                                {
+            char buffer[length + 1];
+            memcpy(buffer, payload, length);
+            buffer[length] = '\0';
+            if (_userCallback)
+                _userCallback(topic, buffer); });
     }
 
     uint16_t publish(const char *topic, const char *msg)
     {
-        return _mqttClient.publish(topic, _qos, _retain, msg); // return packet id, packet is 0 if fail
+        return _mqttClient.publish(topic, msg, _retain) ? 1 : 0;
     }
 };
 
